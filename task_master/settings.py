8bc366f309b26d956e807fced73e000905892924
yulias97 <yulkastetsko@gmail.com>
2024-03-06 19:31:33 +0200

    add task search feature

--- task_master/settings.py
+++ task_master/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taskmanager",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
