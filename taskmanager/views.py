66c8cc5b7bdf9cecb0e3d39f936fe735906685cc
yulias97 <yulkastetsko@gmail.com>
2024-03-06 20:38:08 +0200

    add pagination feature

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -20,6 +20,8 @@ def index(request):
 
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
+    paginate_by = 5
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(TaskListView, self).get_context_data(**kwargs)
 
