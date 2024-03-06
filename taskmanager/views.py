84483a76b06cbc9fbf26e80b0195f035c7d694f8
yulias97 <yulkastetsko@gmail.com>
2024-03-06 16:46:05 +0200

    add basic templates, index view and login/logout functionality

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from taskmanager.models import Task
+
+
+@login_required
+def index(request):
+    total_tasks = Task.objects.count()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    context = {
+        "total_tasks": total_tasks,
+        "completed_tasks": completed_tasks,
+    }
+    return render(request, "taskmanager/index.html", context=context)
