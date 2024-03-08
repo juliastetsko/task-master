c5979dee7d15266444bf00bc2c5ce1eb8e1b95e9
yulias97 <yulkastetsko@gmail.com>
2024-03-08 12:28:15 +0200

    add ModelMultipleChoiceField to TaskForm

--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from taskmanager.models import Task
 
 
 class TaskSearchForm(forms.Form):
@@ -27,3 +30,15 @@ class TaskSearchForm(forms.Form):
         widget=forms.CheckboxInput(attrs={'onclick': 'this.form.submit();'}),
         label="Include completed"
     )
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
